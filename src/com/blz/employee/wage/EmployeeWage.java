88b887f501f3f99dc0f55d8ae8f0ef03f45bba1d
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-09-05 13:56:04 +0530

    Added: Part Time Employee Wage Logic

--- src/com/blz/employee/wage/EmployeeWage.java
+++ src/com/blz/employee/wage/EmployeeWage.java
@@ -8,15 +8,18 @@ public class EmployeeWage {
 
         int wagePerHour = 20;
         int fullDayWorkingHour = 8;
+        int halfDayWorkingHour = 4;
         int salary = 0;
 
         Random random = new Random();
-        int randomNum = random.nextInt(2);
+        int randomNum = random.nextInt(3);
         if(randomNum == 1) {
             System.out.println("Employee is present.");
             salary = wagePerHour * fullDayWorkingHour;
-        }
-        else
+        } else if (randomNum == 2) {
+            System.out.println("Employee is present for the half day.");
+            salary = wagePerHour * halfDayWorkingHour;
+        } else
             System.out.println("Employee is absent.");
         System.out.println("Employee Salary is: "+salary);
     }
